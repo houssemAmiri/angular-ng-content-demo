304a81787208d4ee6fd3258a22b5b03c553af2a2
Houssem Amiri <houssem.amiri@logient.com>
2024-08-06 11:40:52 -0400

    advanced ng content and projection

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,22 @@
 import { Component } from '@angular/core';
 import { WidgetComponent } from './widget/widget.component';
 import { WeatherContentComponent } from './weather/weather-content.component';
+import { WidgetContentDirective } from './widget-content.directive';
 
 @Component({
     selector: 'app-root',
     standalone: true,
-    imports: [WidgetComponent, WeatherContentComponent],
+    imports: [WidgetContentDirective, WidgetComponent, WeatherContentComponent],
     template: `
     <app-widget>
-        <ng-container slot="header">
+        <i slot="action-icon">icon</i>
+        <ng-container ngProjectAs="[header]">
             <div class="widget-title">Weather Forecast</div>
             <div class="widget-sub-title">Currently in Vienna, Austria</div>
         </ng-container>
-        <app-weather-content />
+        <ng-template appWidgetContent>
+            <app-weather-content  slot="content"/>
+        </ng-template>
     </app-widget>
   `
 })
--- src/app/widget/widget.component.ts
+++ src/app/widget/widget.component.ts
@@ -1,19 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, contentChild, signal, TemplateRef } from '@angular/core';
+import { ButtonComponent } from '../button/button.component';
+import { NgTemplateOutlet } from '@angular/common';
+import { WidgetContentDirective } from '../widget-content.directive';
 
 @Component({
     selector: 'app-widget',
     standalone: true,
+    imports: [ButtonComponent, NgTemplateOutlet],
     template: `
     <div class="widget-header">
-      <ng-content select="[slot='header']"/>
+      <ng-content select="[header]"/>
+      <app-button class="action"> 
+            <ng-content ngProjectAs="[slot='icon']" select="[slot='action-icon']" />
+       </app-button>
     </div>
-    <div class="widget-content">
-      <ng-content> 
-        <p class="no-content">No content ......</p>
-      </ng-content>
-    </div>
+
+    @if (!hidden()) {
+        <div class="widget-content">
+                <ng-container [ngTemplateOutlet]="content()?.tpl || noContent"]>
+                </ng-container>
+                <ng-template #noContent select="[slot='content']"> 
+                    <p class="no-content">No content ......</p>
+                </ng-template>
+           
+        </div>
+    }
+    <button (click)="hidden.set(!hidden())">Expand/ Collapse</button>
+
   `,
 })
 export class WidgetComponent {
+    hidden = signal(false)
+    content = contentChild(WidgetContentDirective)
 
 }
